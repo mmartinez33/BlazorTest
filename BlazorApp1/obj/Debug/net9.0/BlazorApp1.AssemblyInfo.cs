da850a59f909824aec74543fc0183a56cab3596b
mmartinez33 <116844183+mmartinez33@users.noreply.github.com>
2026-03-29 21:46:26 -0400

    started building toggles
    Started building toggle buttons to change element status

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e0783fd0b4f66e52cce227ec01977e8f51340d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+943f53c5de03029d772cfa825af65062a16a9fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
